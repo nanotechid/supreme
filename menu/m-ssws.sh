74444c28cdb34c1e573866dc9e3ee9dd8d8ca571
nanotechid <116646460+nanotechid@users.noreply.github.com>
2022-11-05 04:37:01 +0700

    update m-ssws.sh

--- menu/m-ssws.sh
+++ menu/m-ssws.sh
@@ -21,13 +21,13 @@ exit 0
 fi
 clear 
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "\E[0;100;33m      • SS WS/GRPC •          \E[0m"
+echo -e "\E[0;100;33m      • Shadowsocks Account •          \E[0m"
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e ""
-echo -e " [\e[36m•1\e[0m] Create Account SS WS/GRPC "
-echo -e " [\e[36m•2\e[0m] Create Trial SS WS/GRPC "
-echo -e " [\e[36m•3\e[0m] Extending Account SS WS/GRPC Active "
-echo -e " [\e[36m•4\e[0m] Delete Account SS WS/GRPC "
+echo -e " [\e[36m•1\e[0m] Create Account Shadowsocks "
+echo -e " [\e[36m•2\e[0m] Create Trial Shadowsocks "
+echo -e " [\e[36m•3\e[0m] Extending Account Shadowsocks "
+echo -e " [\e[36m•4\e[0m] Delete Account Shadowsocks "
 echo -e ""
 echo -e " [\e[31m•0\e[0m] \e[31mBACK TO MENU\033[0m"
 echo -e   ""
