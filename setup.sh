9ccf38b5e4d157df2b3f45579aa65dfc8ef21481
nanotechid <116646460+nanotechid@users.noreply.github.com>
2022-11-05 03:49:22 +0700

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -242,7 +242,7 @@ echo ""
 echo ""
 echo "   >>> Service & Port"  | tee -a log-install.txt
 echo "   - OpenSSH                  : 22"  | tee -a log-install.txt
-echo "   - SSH WWebsocket           : 80 [ON]" | tee -a log-install.txt
+echo "   - SSH Websocket            : 80 [ON]" | tee -a log-install.txt
 echo "   - SSH SSL Websocket        : 443" | tee -a log-install.txt
 echo "   - Stunnel4                 : 447, 777" | tee -a log-install.txt
 echo "   - Dropbear                 : 109, 143" | tee -a log-install.txt
