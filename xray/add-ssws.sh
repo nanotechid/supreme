76175bf4b058a38bb8b7ed0ebd79de5f5a924d32
nanotechid <116646460+nanotechid@users.noreply.github.com>
2022-11-05 03:48:12 +0700

    update add-ssws.sh

--- xray/add-ssws.sh
+++ xray/add-ssws.sh
@@ -67,7 +67,7 @@ shadowsocks_base64=$(cat /tmp/log)
 echo -n "${shadowsocks_base64}" | base64 > /tmp/log1
 shadowsocks_base64e=$(cat /tmp/log1)
 shadowsockslink="ss://${shadowsocks_base64e}@isi_bug_disini:$tls?path=/ss-ws&security=tls&host=${domain}&type=ws&sni=${domain}#${user}"
-shadowsockslink1="ss://${shadowsocks_base64e}@isi_bug_disini:$tls?path=/ss-ws&security=none&host=${domain}&type=ws#${user}"
+shadowsockslink1="ss://${shadowsocks_base64e}@isi_bug_disini:$ntls?path=/ss-ws&security=none&host=${domain}&type=ws#${user}"
 shadowsockslink2="ss://${shadowsocks_base64e}@${domain}:$tls?mode=gun&security=tls&type=grpc&serviceName=ss-grpc&sni=bug.com#${user}"
 systemctl restart xray
 rm -rf /tmp/log
