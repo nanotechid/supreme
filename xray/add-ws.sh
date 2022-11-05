a60d736992b894b93975e71a8f56cbf38d41db9f
nanotechid <116646460+nanotechid@users.noreply.github.com>
2022-11-05 09:25:48 +0700

    update add-ws.sh

--- xray/add-ws.sh
+++ xray/add-ws.sh
@@ -72,8 +72,8 @@ else
 domain=$IP
 fi
 
-tls="$(cat ~/log-install.txt | grep -w "Vmess TLS" | cut -d: -f2|sed 's/ //g')"
-none="$(cat ~/log-install.txt | grep -w "Vmess None TLS" | cut -d: -f2|sed 's/ //g')"
+tls="$(cat ~/log-install.txt | grep -w "Vmess WS TLS" | cut -d: -f2|sed 's/ //g')"
+none="$(cat ~/log-install.txt | grep -w "Vmess WS none TLS" | cut -d: -f2|sed 's/ //g')"
 until [[ $user =~ ^[a-zA-Z0-9_]+$ && ${CLIENT_EXISTS} == '0' ]]; do
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e "\\E[0;41;36m      Add Vmess Account      \E[0m"
