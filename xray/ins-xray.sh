9e9ae8778ef40077cf214853b75f7f2438d39802
nanotechid <116646460+nanotechid@users.noreply.github.com>
2022-11-07 21:38:57 +0700

    update ins-xray.sh

--- xray/ins-xray.sh
+++ xray/ins-xray.sh
@@ -417,7 +417,6 @@ http {
              ssl_ciphers EECDH+CHACHA20:EECDH+CHACHA20-draft:EECDH+ECDSA+AES128:EECDH+aRSA+AES128:RSA+AES128:EECDH+ECDSA+AES256:EECDH+aRSA+AES256:RSA+AES256:EECDH+ECDSA+3DES:EECDH+aRSA+3DES:RSA+3DES:!MD5;
              ssl_protocols TLSv1.1 TLSv1.2 TLSv1.3;
              root /home/vps/public_html;
-             set_real_ip_from 127.0.0.1;
         }
 EOF
 sed -i '$ ilocation = /vless' /etc/nginx/conf.d/xray.conf
