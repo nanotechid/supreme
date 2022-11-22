e60565d0f7d7ec2a77159af11346faa352c43453
nanotechid <116646460+nanotechid@users.noreply.github.com>
2022-11-23 02:28:16 +0700

    Update add-host.sh

--- ssh/add-host.sh
+++ ssh/add-host.sh
@@ -76,7 +76,7 @@ echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━�
 read -n 1 -s -r -p "Press any key to back on menu"
 menu
 else
-echo "IP=$host" > /var/lib/SIJA/ipvps.conf
+echo "IP=$host" > /var/lib/ipvps.conf
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo "Dont forget to renew cert"
 echo ""
